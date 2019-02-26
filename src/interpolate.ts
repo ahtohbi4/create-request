a01512163929ea1230456f54362ac8860b281200
Alexander Antonov <alexandr-post@yandex.ru>
2019-02-26 15:08:53 +0300

    src/interpolate.ts was updated

--- src/interpolate.ts
+++ src/interpolate.ts
@@ -1,19 +1,36 @@
+type Interpolator = (
+    pattern: string,
+    params?: { [key: string]: number | string },
+) => string;
+
 /**
  * Returns a new string with parameters passed as the second argument or default values.
  *
  * @param {string} pattern
  * @param {object} [params={}]
+ * @returns {string}
+ *
+ * @example
+ *
+ * interpolate('A {foo} is 42 and {baz=BAZ} is BAZ', { foo: 42 });
+ *
+ * // => "A 42 is 42 and BAZ is BAZ"
  */
-export default (
-    pattern: string,
-    params: { [key: string]: number | string } = {},
-): string => pattern.replace(/{([^}]+)}/g, (_match, param: string) => {
-    const [name, defaultValue] = param.split('=', 2);
-    const { [name.trim()]: value = defaultValue } = params;
+const interpolate: Interpolator = (
+    pattern,
+    params = {},
+) => pattern.replace(
+    /{([^}]+)}/g,
+    (_match, param) => {
+        const [name, defaultValue] = <string>param.split('=', 2);
+        const { [name.trim()]: value = defaultValue } = params;
 
-    if (value === undefined) {
-        return `{${param}}`;
-    }
+        if (value === undefined) {
+            return `{${param}}`;
+        }
 
-    return String(value).trim();
-});
+        return String(value).trim();
+    },
+);
+
+export default interpolate;
